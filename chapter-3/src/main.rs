38459152f0f5947bd366108cd2b19f15cfcabdf9
jewel109 <jewelrana176517@gmail.com>
2022-10-18 21:06:43 +0600

    data types added

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -6,9 +6,16 @@ fn main() {
     let x = "six"; // shadowing 
     println!("{}",x);
 
+    
     const SUBSCRIBER_COUNT :u32 = 100_000; // const must be type annotated and not be muted
 
 
+    // data types
+    // scaler and compound
+    // scaler : Interger, Floating point , Boolean, Character
+    // compound type : tuple, array
+
+    
     
 
 
