3c11699f58ea2a81fb65412fc9bed18861484e4d
jewel109 <jewelrana176517@gmail.com>
2022-10-29 08:29:29 +0600

    method learning

--- struct-type/src/main.rs
+++ struct-type/src/main.rs
@@ -1,13 +1,21 @@
 #[derive(Debug)]
-struct Rectangle {
+
+struct Rectangle{
     width: u32,
     height: u32
 }
-fn main() {
-    let rect1 = Rectangle{
-        width: 30,
-        height: 50,
-    };
 
-    println!("rect1 is {:#?}",rect1)
+impl Rectangle{
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let  rect1 = Rectangle{
+        width:20,
+        height:10
+    };
+    println!("rect1.area is {}", rect1.area());
+
 }
